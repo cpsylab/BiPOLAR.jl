906c10ed256cd1b2daf9e2cff7ada1ab716a6ebf
Documenter.jl <documenter@juliadocs.github.io>
2021-08-03 13:36:33 +0000

    build based on 66b9c10

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Lithium","category":"page"},{"location":"#Lithium","page":"Home","title":"Lithium","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Lithium.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Lithium]","category":"page"}]
+}
